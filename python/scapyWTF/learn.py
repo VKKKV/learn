da4dfb874e000d41853bba6e95bea406e483af41
vkkkv <104302767+VKKKV@users.noreply.github.com>
2026-03-03 15:23:16 +0800

    refactor: reorganize scapy scripts and documentation

--- python/scapyWTF/learn.py
+++ python/scapyWTF/learn.py
@@ -1,14 +1,22 @@
 from struct import pack
 
-from scapy.all import sr1, rdpcap, sniff, Raw
-from scapy.all import sniff, sr1, srp
-from scapy.layers.inet import ICMP, IP, UDP, TCP
+from scapy.all import Raw, rdpcap, sniff, sr1, srp
+from scapy.layers.inet import ICMP, IP, TCP, UDP
 from scapy.layers.l2 import ARP, Ether
 
 packet = IP(dst="192.168.1.1") / UDP(dport=80) / b"I use Nixos btw"
 # packet.show()
 packet = IP(dst="192.168.1.1") / TCP(dport=80, flags="S") / b"I use Arch btw"
 # packet.show()
+
+# sr(IP(dst="172.20.29.5/30")/TCP(dport=[21,22,23]),inter=0.5,retry=-2,timeout=1)
+
+# SYN Scans
+# sr1(IP(dst="72.14.207.99")/TCP(dport=80,flags="S"))
+# sr(IP(dst="192.168.1.1")/TCP(sport=666,dport=(440,443),flags="S"))
+# sr(IP(dst="192.168.1.1")/TCP(sport=RandShort(),dport=[440,441,442,443],flags="S"))
+# ans.summary( lambda s,r: r.sprintf("%TCP.sport% \t %TCP.flags%") )
+# srloop(IP(dst="www.target.com/30")/TCP())
 
 
 def myread():
